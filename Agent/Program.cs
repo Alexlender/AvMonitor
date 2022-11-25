294e4200d04fe8a1fbd8a5a4accc658172e90659
Alexander <Alexander@DESKTOP-0381D7M>
2022-11-25 08:53:28 +0700

    Добавьте файлы проекта.

---
+++ Agent/Program.cs
@@ -0,0 +1,46 @@
+﻿using Hangfire;
+using Hangfire.SqlServer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = AgentDb; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False
+
+builder.Services.AddHangfire(h => h.UseSqlServerStorage("Data Source = (localdb)\\MSSQLLocalDB; " +
+       "Initial Catalog = AgentDb; " +
+       "Integrated Security = True; " +
+       "Connect Timeout = 30; " +
+       "Encrypt = False; " +
+       "TrustServerCertificate = False; " +
+       "ApplicationIntent = ReadWrite; " +
+       "MultiSubnetFailover = False"));
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHangfireDashboard();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ AvMonitor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AvMonitor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
