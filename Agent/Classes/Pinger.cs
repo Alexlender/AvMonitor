64587d26a6a5b121a2c69205363e9e7a98d1f67d
Alexlender <sashacharugin@mail.ru>
2022-11-28 18:07:31 +0700

    Add Class Pinger

---
+++ Agent/Classes/Pinger.cs
@@ -0,0 +1,11 @@
+﻿using AvMonitor.Models;
+namespace Agent.Classes
+{
+    public static class Pinger
+    {
+        static public ResponseModel Ping(Uri uri)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- AvMonitor/Models/ResponseModel.cs
+++ AvMonitor/Models/ResponseModel.cs
@@ -4,6 +4,11 @@ namespace AvMonitor.Models
 {
     public class ResponseModel
     {
+        public ResponseModel(HttpStatusCode statusCode)
+        {
+            StatusCode = statusCode;
+            DateTime = DateTime.Now;
+        }
         public HttpStatusCode StatusCode { get; }
 
         public DateTime DateTime { get; }
