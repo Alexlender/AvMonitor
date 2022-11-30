87f713b7401cfce0e2416e7ccc1232c6a3808fdb
medomag2221 <118610256+medomag2221@users.noreply.github.com>
2022-11-30 11:15:50 +0700

    isi

--- Agent/Classes/Pinger.cs
+++ Agent/Classes/Pinger.cs
@@ -1,11 +1,19 @@
-﻿using AvMonitor.Models;
+using AvMonitor.Models;
+using System.Net;
 namespace Agent.Classes
 {
     public static class Pinger
     {
-        static public ResponseModel Ping(Uri uri)
+        static public int Ping(Uri uri)
         {
-            throw new NotImplementedException();
+            // Creates an HttpWebRequest for the specified URL. 
+            HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+            // Sends the HttpWebRequest and waits for a response.
+            HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+            if (myHttpWebResponse.StatusCode == HttpStatusCode.OK) //аналог 200 в перечислении
+                return 1;
+            else
+                return -1;
         }
     }
 }
