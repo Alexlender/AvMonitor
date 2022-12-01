db2c1abdbdac25cbc55b8d76e0c929c095ba0262
medomag2221 <medomag2221@gmail.com>
2022-12-01 12:37:45 +0700

    pinger at all

--- Agent/Classes/Pinger.cs
+++ Agent/Classes/Pinger.cs
@@ -1,11 +1,18 @@
 ﻿using AvMonitor.Models;
+using System;
+using System.Net;
 namespace Agent.Classes
 {
     public static class Pinger
     {
         static public ResponseModel Ping(Uri uri)
         {
-            throw new NotImplementedException();
+            // Создаёт HttpWebRequest объект для URL
+            HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+            //Отправляет HttpWebRequest и ждёт ответ в виде response
+            HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+            ResponseModel RM = new ResponseModel(myHttpWebResponse.StatusCode);
+            return RM;
         }
     }
 }
