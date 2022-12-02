c408fd3c43adf135ca273641bf6b96a1dbfeabf5
aikame <asus1172@gmail.com>
2022-12-03 01:59:50 +0700

    культуризировано

--- AvMonitor/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ AvMonitor/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -79,7 +79,7 @@ namespace AvMonitor.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Display(Name = "Поставить клеймо?")]
+            [Display(Name = "НИ АНАНИМНА?")]
             public bool RememberMe { get; set; }
         }
 
