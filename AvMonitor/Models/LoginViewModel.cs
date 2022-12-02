1449f105ee4849a68a9395f5d80be427f0e6570d
svmf0xx <foxikmain@gmail.com>
2022-12-02 13:27:05 +0700

    Я жёстко сделал новую модель! ))))))

---
+++ AvMonitor/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AvMonitor.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Логин")]
+        public string UserName { get; set; }
+        [Required]
+        [Display(Name = "Не логин (пароль)")]
+        public string Password { get; set; }    
+    }
+}
