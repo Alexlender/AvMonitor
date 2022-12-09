03047f72e96dadad038ff9a80247f212bc0537bd
Alexlender <sashacharugin@mail.ru>
2022-12-09 23:40:17 +0700

    Я высрал что-то

--- AvMonitor/Controllers/ResponsesController.cs
+++ AvMonitor/Controllers/ResponsesController.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AvMonitor.Data;
+using AvMonitor.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AvMonitor.Controllers
 {
     public class ResponsesController : Controller
     {
-        public IActionResult Index()
+        private readonly TaskDataContext _context;
+
+        public ResponsesController(TaskDataContext context)
         {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index(TaskModel task)
+        {
+            ViewData["dbContext"] = _context;
+            ViewData["task"] = task;
+
             return View();
         }
     }
