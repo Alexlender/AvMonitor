5dbd23d2083494ea4e71ea7c54d16340e13b2b14
aikame <asus1172@gmail.com>
2022-12-10 00:10:40 +0700

    виу-виу

--- AvMonitor/Controllers/TaskController.cs
+++ AvMonitor/Controllers/TaskController.cs
@@ -29,12 +29,12 @@ namespace AvMonitor.Controllers
                     db.AddTask(task);
                     string result = (await HttpManager.GetInstance().PostAsync("Task/add", task)).ToString();
                     Console.WriteLine(result);
-                    return Redirect("/TaskEdit");
+                    return Redirect("/Home/TaskEdit");
                 }
                 
             }
             Console.WriteLine("ERROR");
-            return Redirect("/TaskEdit");
+            return Redirect("/Home/TaskEdit");
 
         }
 
@@ -44,7 +44,7 @@ namespace AvMonitor.Controllers
             task.UserName = User.Identity?.Name;
             db.DeleteTaskByID(task.Id);
             Console.WriteLine(await HttpManager.GetInstance().DeleteAsync($"Task/delete/{task.Id}"));
-            return Redirect("/TaskEdit");
+            return Redirect("/Home/TaskEdit");
         }
 
     }
